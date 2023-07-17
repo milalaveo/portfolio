f9913a4dc76158edbb58c0dceadda7d4bd4c8e5e
milalaveo <milalaveo@gmail.com>
2023-07-17 14:48:02 +0200

    marquee fix

--- themes/milalaveo/assets/js/script.js
+++ themes/milalaveo/assets/js/script.js
@@ -10,4 +10,25 @@ document.addEventListener('DOMContentLoaded', function () {
       document.body.style.overflow = e.target.checked ? 'hidden' : 'initial';
     })
   }
+
+  var marquees = document.getElementsByClassName('marquee');
+
+  for (var i = 0; i < marquees.length; i++) {
+    var width = marquees[i].getBoundingClientRect().width;
+    var inners = marquees[i].getElementsByTagName('span');
+
+    if (!inners.length) {
+      return;
+    }
+
+    var inner = inners[0];
+    var innerWidth = inner.getBoundingClientRect().width;
+
+    console.log(width, innerWidth);
+    const requiredSize = Math.ceil(width / innerWidth) + 1;
+
+    for (var j = 0; j < requiredSize; j++) {
+      marquees[i].appendChild(inner.cloneNode(true));
+    }
+  }
 })
