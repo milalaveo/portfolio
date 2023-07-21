e053864ff18fe8316e8e4ce03020b7279f246993
milalaveo <milalaveo@gmail.com>
2023-07-21 10:53:59 +0200

    themes for kbs and mighty

--- themes/milalaveo/assets/js/script.js
+++ themes/milalaveo/assets/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var burgers = document.getElementsByClassName('burger-trigger');
+  var burgers = document.getElementsByClassName('burger__trigger');
 
   for (var i = 0; i < burgers.length; i++) {
     burgers[i].addEventListener('change', function (e) {
